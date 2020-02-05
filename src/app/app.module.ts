0fb4c535c6095b44bb3dd4d2932bfa3a2420e514
vienendelsur <isola.patrick@gmail.com>
2020-02-05 11:42:18 +0100

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JeuComponent } from './jeu/jeu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JeuComponent
   ],
   imports: [
     BrowserModule,
