b87a7af45e12d7745651b9874d0909d957afdd3a
vienendelsur <isola.patrick@gmail.com>
2020-02-05 15:26:49 +0100

    style et fin

--- src/app/jeu/jeu.component.ts
+++ src/app/jeu/jeu.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JeuComponent implements OnInit {
 
+  // propriétés d'instances
+  hasard: number;
+  propostion: number;
+  tentatives: number;
+  message: string;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.hasard = Math.floor( Math.random() * 100 ) + 1;
+    console.log(this.hasard);
+    this.tentatives = 0;
+    this.message =  "Devine le nombre !";
+
   }
 
+  //méthode d'instance pour contrôler le déroulement du jeu
+  jouer() {
+    //to do, à faire todolist
+    if(this.proposition > this.hasard) {
+      this.message = "C'est plus petit !";
+      this.tentatives += 1;
+    }
+
+    if(this.proposition < this.hasard) {
+      this.message = "C'est plus grand !";
+      this.tentatives += 1;
+    }
+
+    if(this.proposition === this.hasard) {
+      this.message = "Bravo mon Kiki !";
+    }
+  }
 }
